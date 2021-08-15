0ff322e53ce578b7ac2c1d59d99b2b42ee78d885
Troy Dorman <troydorman@gmail.com>
2021-08-14 20:25:06 -0600

    refactored the code to be even shorter

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const current = document.querySelector("#current");
-const imgs = document.querySelectorAll(".imgs img");
-
+const [current, imgs] = [
+  document.querySelector("#current"),
+  document.querySelectorAll(".imgs img"),
+];
 imgs.forEach((img) =>
   img.addEventListener("click", (e) => (current.src = e.target.src))
 );
