10d07951664c5e8cf3d72b11b7858d3e7be945b9
Troy Dorman <troydorman@gmail.com>
2021-08-14 20:28:24 -0600

    added opacity to currently-clicked image thumbnail

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ const [current, imgs] = [
   document.querySelector("#current"),
   document.querySelectorAll(".imgs img"),
 ];
+
+const opacity = 0.4;
+
 imgs.forEach((img) => img.addEventListener("click", imgClick));
 
 function imgClick(e) {
+  // reset the opacity
+  imgs.forEach((img) => (img.style.opacity = 1));
+
+  // change current image to src of clicked image
   current.src = e.target.src;
+
+  // change the opacity to opacity var
+  e.target.style.opacity = opacity;
 }
