332a4db2db84cba5e856e90b7c2e7c1cf4439a13
Troy Dorman <troydorman@gmail.com>
2021-08-14 20:26:25 -0600

    refactoring even further by creating an img click function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const [current, imgs] = [
   document.querySelector("#current"),
   document.querySelectorAll(".imgs img"),
 ];
-imgs.forEach((img) =>
-  img.addEventListener("click", (e) => (current.src = e.target.src))
-);
+imgs.forEach((img) => img.addEventListener("click", imgClick));
+
+function imgClick(e) {
+  current.src = e.target.src;
+}
